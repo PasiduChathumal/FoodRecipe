014b0038c98909a78cb0ba8465e67186486fe0d0
COBSCCOMPY4231P-032 <COBSCCOMPY4231P-032@NIBM-LAB04-PC03.local>
2024-09-28 14:44:11 +0530

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food-Recipe
+//
+//  Created by COBSCCOMPY4231P-032 on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_RecipeApp.swift
+//  Food-Recipe
+//
+//  Created by COBSCCOMPY4231P-032 on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct Food_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
